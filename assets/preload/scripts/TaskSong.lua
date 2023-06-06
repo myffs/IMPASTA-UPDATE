52338ca62f3eb32125eb1393cc3726b742f7b861
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-06-06 12:21:31 -0400

    new song card + update

--- assets/preload/scripts/TaskSong.lua
+++ assets/preload/scripts/TaskSong.lua
@@ -1,32 +1,31 @@
 function onCreatePost()
-    if checkFileExists('data/'..songPath..'/info.txt') then
-        --debugPrint('it exists');
         makeLuaText('text', getTextFromFile('data/'..songPath..'/info.txt'), 0, 0, 200);
-        setProperty('text.alignment', 'left');
-        setProperty('text.size', 24);
-        setProperty('text.font', 'arial.ttf');
-        setProperty('text.borderSize', 1);
-        setObjectCamera('text', 'camOther');
+        setTextAlignment('text', 'left');
+        setTextSize('text', 24)
+        setTextFont('text', 'arial.ttf');
+        setTextBorder('text', 1)
+        setObjectCamera('text', 'other');
         addLuaText('text');
 
-        size = getProperty('text.width')
+        size = getTextWidth('text')
 
         makeLuaSprite('creditbg', nil, size - 12, 200 - 12);
-        makeGraphic('creditbg', size + 24, getProperty('text.height') + 24, 'FFFFFF');
-        setObjectCamera('creditbg', 'camOther');
+        makeGraphic('creditbg', size + 24, getProperty('text.height') + 24, '000000');
+        setObjectCamera('creditbg', 'other');
         setProperty('creditbg.alpha', 0.47);
         addLuaSprite('creditbg');
 
         setProperty('creditbg.x', -size);
         setProperty('text.x', -size);
-    end
 end
+
 function onCountdownTick(counter)
     if counter == 0 then
         doTweenX('bgthingin', 'creditbg', 0, 1, 'quintInOut');
         doTweenX('textthingin', 'text', 0, 1, 'quintInOut');
     end 
 end
+
 function onTweenCompleted(tag)
     if tag == 'bgthingin' then
         runTimer('bgthingout', 2);
@@ -36,6 +35,7 @@ function onTweenCompleted(tag)
         removeLuaText('text', true);
     end
 end
+
 function onTimerCompleted(tag)
     if tag == 'bgthingout' then
         doTweenX('bgthingout', 'creditbg', -size - size - 50, 1, 'quintInOut');
