1e062fa06b16d928bbae7c0980e8e2c6755cd6ef
LilDrippyMyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-07-02 21:47:06 -0400

    white sprites, new bg for hotted dog.

---
+++ assets/preload/stages/Void.lua
@@ -0,0 +1,23 @@
+
+--How makeLuaSprite works:
+--makeLuaSprite(<SPRITE VARIABLE>, <SPRITE IMAGE FILE NAME>, <X>, <Y>);
+--"Sprite Variable" is how you refer to the sprite you just spawned in other methods like "setScrollFactor" and "scaleObject" for example
+
+--so for example, i made the sprites "stagelight_left" and "stagelight_right", i can use "scaleObject('stagelight_left', 1.1, 1.1)"
+--to adjust the scale of specifically the one stage light on left instead of both of them
+
+function onCreate()
+	-- background shit
+	makeLuaSprite('VoidFront', 'VoidFront', 0, 90);
+	setScrollFactor('VoidFront', 0.9, 0.9);
+	scaleObject('VoidFront', 0.9, 0.9);
+
+
+	makeLuaSprite('VoidBack', 'VoidBack', 0, 0);
+	setScrollFactor('VoidBack', 0.9, 0.9);
+
+	
+
+	addLuaSprite('VoidBack', false);
+	addLuaSprite('VoidFront', true);
+end
